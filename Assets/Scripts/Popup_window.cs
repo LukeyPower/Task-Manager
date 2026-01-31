dc7e126e8186478ca9dcfb372dacec216955dccc
ArtLuke <lfrons@hotmail.com>
2026-01-31 18:43:20 +0930

    sik kids

--- Assets/Scripts/Popup_window.cs
+++ Assets/Scripts/Popup_window.cs
@@ -3,24 +3,29 @@ using UnityEngine;
 public class Popup_window : MonoBehaviour
 {
 public AudioClip popdownSound;
+private AudioSource audioSource;
+private GameObject audiomanager;
     public void ClosePopup()
     {
         Destroy(gameObject);
-        //play popdown sound 2d
-        //Unannote below ------
-        //AudioSource.PlayClipAtPoint(popdownSound, Camera.main.transform.position);
+        audioSource.PlayOneShot(popdownSound, 1.0f);
+  
         
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
+
+        audiomanager = GameObject.Find("AudioManager");
+        audioSource = audiomanager.GetComponent<AudioSource>();
         //Unannote below ------
-       // popdownSound = Resources.Load<AudioClip>("popdown");
+        popdownSound = Resources.Load<AudioClip>("popdown");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
